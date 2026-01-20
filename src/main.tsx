8981d7385e6c53c85a34010d1d717e973a2f37cf
Alisson Pereira <alissonpereira@MacBook-Pro-de-Alisson.local>
2026-01-19 21:13:36 -0300

    Main.ts fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { AppProviders } from "./app/providers/AppProviders";
-import { worker } from "./mocks/browser";
+import { AppProviders } from "@/app/providers/AppProviders";
+import { worker } from "@/mocks/browser";
 
 async function enableMocking() {
-  if (import.meta.env.DEV) {
-    await worker.start({ onUnhandledRequest: "bypass" });
+  // liga MSW no dev OU quando você setar a env var no deploy
+  const shouldMock =
+    import.meta.env.DEV || import.meta.env.VITE_ENABLE_MSW === "true";
+
+  if (shouldMock) {
+    await worker.start({
+      serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js` },
+      onUnhandledRequest: "bypass",
+    });
   }
 }
 
